4b0e5ab59a1881953fa85296b0ee175b40a8ffa7
Marek <masitko@gmail.com>
2024-06-02 13:16:30 +0100

    added user registration and authentication with JWT token

--- app/Http/Middleware/CheckAuthenticationToken.php
+++ app/Http/Middleware/CheckAuthenticationToken.php
@@ -3,23 +3,46 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Contracts\Auth\UserProvider;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Auth;
+use ReallySimpleJWT\Token;
+
 
 class CheckAuthenticationToken
 {
-    /**
-     * CHecking if the incoming request has a valid 'Authorization' header.
-     *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
-     * @return \Symfony\Component\HttpFoundation\Response|\Illuminate\Http\JsonResponse
-     */
-    public function handle(Request $request, Closure $next): Response|JsonResponse
-    {
-        if( $request->header('Authorization') !== env('AUTHORIZATION_TOKEN') ) {
-          return response()->json(['error' => 'Unauthorized'], 401);
-        }
-        return $next($request);
+
+  private UserProvider $provider;
+
+  public function __construct()
+  {
+    $this->provider = Auth::createUserProvider('users');
+  }
+
+  /**
+   * CHecking if the incoming request has a valid 'Authorization' header.
+   *
+   * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+   * @return \Symfony\Component\HttpFoundation\Response|\Illuminate\Http\JsonResponse
+   */
+  public function handle(Request $request, Closure $next): Response|JsonResponse
+  {
+    $authHeader = $request->header('Authorization');
+    if ($tokenExists = $authHeader && str_contains($authHeader, 'Bearer ')) {
+      $token = explode(' ', $authHeader)[1];
     }
+    if ($tokenExists && Token::validate($token, Config::get('jwt.secret'))) {
+      $payload = Token::getPayload($token);
+      if( isset($payload['user_id']))
+        Auth::setUser($this->provider->retrieveById($payload['user_id']));
+      $response =  $next($request);
+      return $response;
+    }
+    return response()->json([
+      'error' => 'Unauthorized',
+    ], 401);
+  }
 }
--- routes/api.php
+++ routes/api.php
@@ -4,20 +4,39 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\QuotesController;
 use App\Http\Middleware\CheckAuthenticationToken;
 
+use App\Http\Controllers\AuthController;
+use App\Http\Middleware\JsonResponseMiddleware;
+
+
 /**
- * 
- * V1 API Routes Group so that we can have a versioned API.
- * 
- * Using the CheckAuthenticationToken middleware to check if the incoming request has a valid 'Authorization' header.
+ * Middleware group to ensure that all responses are in JSON format.
  * 
  */
+Route::middleware([JsonResponseMiddleware::class])->group(function () {
+
+/**
+ * V1 API Routes Group so that we can have a versioned API.
+ * 
+ * Using the CheckAuthenticationToken middleware to make sure the incoming request has a valid 'Authorization' header.
+ */
 Route::group(
-  [
-    'prefix' => 'v1',
-    'middleware' => [CheckAuthenticationToken::class]
-  ],
-  function () {
-    Route::get('/quotes', [QuotesController::class, 'index'])->name('quotes.index');
-    Route::put('/quotes/refresh', [QuotesController::class, 'refresh'])->name('quotes.refresh');
-  }
-);
+    [
+      'prefix' => 'v1',
+      'middleware' => [CheckAuthenticationToken::class]
+    ],
+    function () {
+      Route::get('/quotes', [QuotesController::class, 'index'])->name('quotes.index');
+      Route::put('/quotes/refresh', [QuotesController::class, 'refresh'])->name('quotes.refresh');
+    }
+  );
+
+  Route::group(
+    [
+      'prefix' => 'auth',
+    ],
+    function () {
+      Route::post('/register', [AuthController::class, 'register'])->name('auth.register');
+      Route::post('/login', [AuthController::class, 'login'])->name('auth.login');
+    }
+  );
+});
